23572479d4de5b2580144c09448e5cd414c96146
Daniel Whooley <danieljwhooley@gmail.com>
2024-11-11 16:20:02 -0500

    Project Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Microcontrollers", "Embedded Systems", "RTOS", "IoT development","FPGAs"],
+    strings: ["Microcontrollers", "Embedded Software", "Real-Time Operating Systems", "Internet of Things","FPGAs"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
